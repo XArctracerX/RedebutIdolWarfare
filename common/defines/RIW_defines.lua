19cf4072be3acf58972d55a82607c65a359cd708
Arc <146809110+XArctracerX@users.noreply.github.com>
2025-08-22 17:42:05 +0930

    Full Map Done

--- common/defines/RIW_defines.lua
+++ common/defines/RIW_defines.lua
@@ -1,5 +1,5 @@
-NDefines.NGame.START_DATE = "2020.1.1.1"
-NDefines.NGame.END_DATE = "2020.1.1.1"
+NDefines.NGame.START_DATE = "2026.1.1.1"
+NDefines.NGame.END_DATE = "2026.1.1.1"
 
 NDefines.NCountry.MIN_STABILITY = -1.0
 NDefines.NCountry.MIN_WAR_SUPPORT = -1.0
